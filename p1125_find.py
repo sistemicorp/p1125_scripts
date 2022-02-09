7d2a1b9b0e9a407c3eeec608b2db7590a1f67f8f
sistemicorp <martin.guthrie@sistemi.ca>
2022-02-09 13:06:51 -0500

    update copyright date

--- p1125_find.py
+++ p1125_find.py
@@ -3,7 +3,7 @@
 """
 MIT License
 
-Copyright (c) 2021 sistemicorp
+Copyright (c) 2021-2022 sistemicorp
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
